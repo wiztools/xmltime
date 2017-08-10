c6a750eb7c9f5fa337e174b7b0b74ccc12b1fa53
Subhash Chandran <subwiz@gmail.com>
2017-08-10 11:53:05 +0530

    Added comments; modularised code.

--- xmltime.go
+++ xmltime.go
@@ -1,3 +1,8 @@
+/*
+Package xmltime provides struct XMLTime. XMLTime wraps `time.Time` type. XMLTime
+can be used to convert date-time in RFC3339 format in XML element values / attributes
+when unmarshalling.
+*/
 package xmltime
 
 import (
@@ -16,23 +21,21 @@ func mustAddZ(v string) bool {
 	return false
 }
 
-// AllowEmptyDateTime empty dates to: 0001-01-01 00:00:00 +0000 UTC
+/*
+AllowEmptyDateTime sets up the unmarshalling engine to convert empty dates in XML
+to: 0001-01-01 00:00:00 +0000 UTC. In the absence of this flag being set, the parser
+will terminate parsing and return error. This is a irreversible call, and a global one.
+*/
 func AllowEmptyDateTime() {
 	raiseParseErrOnEmpty = false
 }
 
-// XMLTime comment
+// XMLTime wraps time.Time.
 type XMLTime struct {
 	time.Time
 }
 
-// UnmarshalXML comment
-func (t *XMLTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	var v string
-	err := d.DecodeElement(&v, &start)
-	if err != nil {
-		return err
-	}
+func (t *XMLTime) unmarshall(v string) error {
 	if mustAddZ(v) {
 		v += "Z"
 	}
@@ -49,23 +52,25 @@ func (t *XMLTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	return nil
 }
 
-// UnmarshalXMLAttr comment
-func (t *XMLTime) UnmarshalXMLAttr(attr xml.Attr) error {
-	v := attr.Value
-	if mustAddZ(v) {
-		v += "Z"
-	}
-	parse, err := time.Parse(time.RFC3339, attr.Value)
+// UnmarshalXML converts XML element value to xmltime.XMLTime.
+func (t *XMLTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var v string
+	err := d.DecodeElement(&v, &start)
 	if err != nil {
 		return err
 	}
-	*t = XMLTime{parse}
-	return nil
+	return t.unmarshall(v)
+}
+
+// UnmarshalXMLAttr converts attribute value to xmltime.XMLTime.
+func (t *XMLTime) UnmarshalXMLAttr(attr xml.Attr) error {
+	v := attr.Value
+	return t.unmarshall(v)
 }
 
 var beginningTime = time.Time{}
 
-// IsBeginning comment
+// IsBeginning returns true if date-time is set to: 0001-01-01 00:00:00 +0000 UTC.
 func (t *XMLTime) IsBeginning() bool {
 	if t.Equal(beginningTime) {
 		return true
